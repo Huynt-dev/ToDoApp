84c35f30bd00ec5a52a25e5e0489f6d71daf0fb3
HuyNT <huynt.me@gmail.com>
2020-09-04 14:05:19 +0700

    update lowdb

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require('express');
 const bodyParser = require('body-parser')
 const low = require('lowdb')
@@ -15,12 +10,6 @@ db.defaults({ listToDo: []})
   .write()
 
 const app = express();
-var listToDo = [
-  {id:1,task:'Đi chợ'},
-  {id:2,task:'Nấu cơm'},
-  {id:3,task:'Rửa bát'},
-  {id:4,task:'Học code tại CodersX'}
-]
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.use(bodyParser.json()) // for parsing application/json
@@ -29,12 +18,12 @@ app.set('view engine','pug');
 app.set('views','./views');
 
 app.get('/', function(req,res){
-  res.render('index', {listToDo});
+  res.render('index', {listToDo:db.get('listToDo').value()});
 })
 
 app.get('/search', function(req,res){
   var q = req.query.q;
-  var filterTask = listToDo.filter(function(x){
+  var filterTask = db.get('listToDo').value().filter(function(x){
     return x.task.toLowerCase().indexOf(q.toLowerCase()) !== -1;
   });
   res.render('index', {
@@ -48,11 +37,9 @@ app.get('/create', function(req,res){
 })
 
 app.post('/create', function(req,res){
-  listToDo.push(req.body)
+  db.get('listToDo').push(req.body).write()
   res.redirect('/')
 })
 
 // listen for requests :)
-app.listen(process.env.PORT, () => {
-  console.log("Server listening on port " + process.env.PORT);
-});
+app.listen(3000);
